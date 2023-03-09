6579aa2e003e053cf3e45312b3cb2898986b15b5
mmajeed1 <126624972+Kingmoe7864@users.noreply.github.com>
2023-03-09 00:01:56 +0000

    lab 10 exercise 4

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     # Comlete your code here
     ##################################################
     pass
-
+celcius = int(input('enter the Celcius Degree: '))
+fahrenheit = (9.0 / 5.0) * celcius + 32
+print (f'The fahrenheit is {fahrenheit:.2f}')
 
 if __name__ == '__main__':
     main()
